619f487cc21370af8e84f251415701fdecae1807
chiaoicchi <182973681+chiaoicchi@users.noreply.github.com>
2026-02-19 04:02:42 +0900

    settings of Nix and Neovim

---
+++ README.md
@@ -0,0 +1,27 @@
+# Math & Optimization Algorithms for Rust
+
+## Environment
+
+- Rust 1.89.0 (pinned via [fenix](https://github.com/nix-community/fenix))
+- Nix flake for reproducible development environment
+
+## Setup
+
+### Prerequisites
+
+- [Nix](https://nixos.org/) with flakes enabled
+- [direnv](https://github.com/direnv/direnv) (optional)
+
+### Getting Started
+
+Enter the development environment:
+
+```bash
+nix develop
+```
+
+Or, if you have direnv set up:
+
+```bash
+direnv allow
+```
---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "math_optim_rs environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        rustToolchain =
+          (fenix.packages.${system}.toolchainOf {
+            channel = "1.89.0";
+            sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
+          }).withComponents
+            [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rust-analyzer"
+              "rustc"
+              "rustfmt"
+            ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+          ];
+
+          RUST_BACKTRACE = "1";
+
+          shellHook = ''
+            echo "math_optim_rs Environment (fenix)"
+            echo "  Rust $(rustc --version)"
+          '';
+        };
+      }
+    );
+}
