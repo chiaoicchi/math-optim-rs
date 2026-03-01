75a33d74ebf4a632a95288fde0fcc36d400bb882
chiaoicchi <182973681+chiaoicchi@users.noreply.github.com>
2026-03-01 21:38:59 +0900

    feat: primitive root and factorize

--- crates/algebrae/src/num_theory/mod.rs
+++ crates/algebrae/src/num_theory/mod.rs
@@ -3,5 +3,5 @@ mod prime;
 mod sieve;
 
 pub use gcd::{ext_gcd, gcd, lcm};
-pub use prime::is_prime;
+pub use prime::{factorize, is_prime, primitive_root};
 pub use sieve::eratosthenes::SieveEratosthenes;
--- crates/algebrae/src/num_theory/prime.rs
+++ crates/algebrae/src/num_theory/prime.rs
@@ -1,4 +1,5 @@
 use crate::modular::pow_mod;
+use crate::num_theory::gcd;
 
 /// Returns whether given value is prime.
 ///
@@ -48,4 +49,100 @@ fn miller_rabin<const N: usize>(n: u64, d: u64, r: u32, witnesses: &[u64; N]) ->
         }
     }
     true
+}
+
+/// Factorizes given value.
+///
+/// # Complexity
+/// Time: O(n^{1/4} log n), Space: O(log n)
+pub fn factorize(mut n: u64) -> Vec<(u64, u32)> {
+    assert!(n > 0, "n must not be zero");
+    if n == 1 {
+        return Vec::new();
+    }
+    let two = n.trailing_zeros();
+    let mut res = Vec::new();
+    if two > 0 {
+        res.push((2, two));
+        n >>= two;
+    }
+    let mut three = 0;
+    while n % 3 == 0 {
+        three += 1;
+        n /= 3;
+    }
+    if three > 0 {
+        res.push((3, three));
+    }
+    if n == 1 {
+        return res;
+    }
+
+    let mut factors = vec![n];
+    let mut i = 0;
+    unsafe {
+        while i < factors.len() {
+            let n = *factors.get_unchecked(i);
+            if is_prime(n) {
+                i += 1;
+                continue;
+            }
+
+            'LOOP: for t in 1.. {
+                let mut x: u64 = t;
+                let mut y = ((x as u128 * x as u128 + t as u128) % n as u128) as u64;
+                loop {
+                    let g = gcd(x.abs_diff(y), n);
+                    if g == n {
+                        break;
+                    }
+                    if g != 1 {
+                        *factors.get_unchecked_mut(i) /= g;
+                        factors.push(g);
+                        break 'LOOP;
+                    }
+                    x = ((x as u128 * x as u128 + t as u128) % n as u128) as u64;
+                    y = ((y as u128 * y as u128 + t as u128) % n as u128) as u64;
+                    y = ((y as u128 * y as u128 + t as u128) % n as u128) as u64;
+                }
+            }
+        }
+    }
+
+    factors.sort_unstable();
+    let mut i = 0;
+    let len = factors.len();
+    unsafe {
+        let f = factors.as_ptr();
+        while i < len {
+            let p = *f.add(i);
+            let mut j = i + 1;
+            while j < len && *f.add(j) == p {
+                j += 1;
+            }
+            res.push((p, (j - i) as u32));
+            i = j;
+        }
+    }
+    res
+}
+
+/// Returns primitive root of prime number p.
+///
+/// # Complexity
+/// Time: practically small, Space: O(log p)
+pub fn primitive_root(p: u64) -> u64 {
+    if p == 2 {
+        return 1;
+    }
+    let factors = factorize(p - 1);
+    'LOOP: for g in 2..p {
+        for (f, _) in &factors {
+            if pow_mod(g, (p - 1) / f, p) == 1 {
+                continue 'LOOP;
+            }
+        }
+        return g;
+    }
+    unreachable!();
 }
